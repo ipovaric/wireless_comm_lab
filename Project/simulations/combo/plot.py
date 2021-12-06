d027e5d3a28634bbaba78ff9621d48791e7daad3
Igor <ipovaric@gmail.com>
2021-12-05 20:15:18 -0700

    added combination simulations

---
+++ Project/simulations/combo/plot.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+from pandas.api.types import CategoricalDtype
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+pdr = pd.read_csv('./results/all.csv', sep=';')
+
+pdr['encoding'] = pdr.encoding.astype(CategoricalDtype(categories=range(8), ordered=True))
+# pdr['noise'] = pdr.noise.astype(CategoricalDtype(categories=range(11), ordered=True))
+
+pdr.encoding.cat.categories = [
+        "BPSK 1/2", "BPSK 3/4",
+        "QPSK 1/2", "QPSK 3/4",
+        "16-QAM 1/2", "16-QAM 3/4",
+        "64-QAM 2/3", "64-QAM 3/4"]
+
+# pdr.noise.cat.categories = ["0.0","0.1","0.2","0.3","0.4","0.5","0.6","0.7",
+#                             "0.8","0.9","10.0"]
+
+a = pdr.groupby(['encoding', 'snr'])
+
+b = a.agg({'received': np.mean}).reset_index()
+c = b.pivot(index='snr', columns='encoding', values='received')
+c.plot()
+plt.ylabel('Ratio of received packets')
+plt.title('802.11p with AWGN Model')
+plt.legend(loc='right')
+plt.ylim([-10,110])
+plt.savefig('pdr.pdf')
+
+# b.to_csv('run1.csv')
