124c94330703aee13c0a26a24a37f6b351426529
Igor <ipovaric@gmail.com>
2021-12-05 20:17:38 -0700

    adding project files for presi

--- startup.bat
+++ startup.bat
@@ -1,4 +1,4 @@
 :: wt nt -p "Ubuntu-20.04" # "startingDirectory": "\\\\wsl.localhost\\Ubuntu-20.04\\home\\ipova"
 wt nt -p "Ubuntu-20.04"
 
-"C:\Program Files\VcXsrv\xlaunch.exe"
+"C:\Users\ipova\Google Drive\Fall 2021\CompEng5430 Wireless Networks\wireless_comm_lab\config.xlaunch"
